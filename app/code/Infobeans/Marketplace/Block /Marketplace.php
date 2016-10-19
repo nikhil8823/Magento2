33a6a993384dd91137afc9a88c5d4e7eb5eae0ba
nikhil-infobeans <nikhil.kakde@infobeans.com>
2016-10-19 16:09:30 +0530

    Marketplace module structure

---
+++ app/code/Infobeans/Marketplace/Block /Marketplace.php
@@ -0,0 +1,10 @@
+<?php
+namespace Infobeans\Marketplace\Block;
+ 
+class Marketplace extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    { die("innnn");
+        return 'Hello world!';
+    }
+}
